c661892aeffcc73b83c1195562829d02deec5907
Serhii Voloshchuk <160412800+Serhii-Voloshchuk@users.noreply.github.com>
2024-05-15 08:21:47 +0300

    fixed-15.05.2024

--- js/task-4.js
+++ js/task-4.js
@@ -6,18 +6,17 @@ switch(country){
     case "Australia":
         return "Shipping to Australia will cost 170 credits";
         break;
-  
     case "China":
         return "Shipping to China will cost 100 credits";
         break;
     case "Chile":
         return "Shipping to Chile will cost 250 credits";
         break;
+    case "Jamaica":
+        return "Shipping to Jamaica will cost 120 credits";
+        break;
 
-    return "Shipping to Jamaica will cost 120 credits";
-    break;
-
-      default:
+    default:
         return "Sorry, there is no delivery to your country";
 
 }
